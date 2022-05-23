677072fefd11b5e43a458614b01c67d0852d7815
mahmudul1995 <mahmuduls451@gmail.com>
2022-05-23 20:09:25 +0600

    part get api create

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const objectId = require('mongodb').ObjectId;
 const cors = require('cors');
 const port = process.env.PORT || 5000
 
@@ -21,6 +22,23 @@ async function run() {
     try{
         await client.connect();
         console.log('Database connect sucess');
+        const partsCollection = client.db("auto_parts").collection("single_parts");
+
+
+        app.get('/single_parts', async (req, res) => {
+            const parts = await partsCollection.find({}).toArray();
+            res.send(parts)
+        })
+
+        app.get('/single_parts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: objectId(id) };
+            const result = await partsCollection.findOne(query);
+            res.send(result);
+        });
+
+
+
     }
 
     finally{

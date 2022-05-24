5208f3dfc404b6a29e3bfabf46db63d6046a5c10
mahmudul1995 <mahmuduls451@gmail.com>
2022-05-25 02:10:30 +0600

    only user make admin api create

--- index.js
+++ index.js
@@ -48,6 +48,20 @@ async function run() {
     }
 
 
+    const verifyAdmin = async (req, res,next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({ email: requester });
+      if (requesterAccount.role === 'admin') {
+        next();
+      }
+
+      else{
+        res.status(403).send({message: 'forbidden'});
+      }
+
+    }
+
+
     app.post("/login", async (req, res) => {
       const email = req.body;
 
@@ -132,7 +146,7 @@ async function run() {
 
 
 
-    app.put('/user/admin/:email',verifyJWT, async (req, res) => {
+    app.put('/user/admin/:email',verifyJWT,verifyAdmin, async (req, res) => {
         const email = req.params.email;
           const filter = { email: email };
           const updateDoc = {
@@ -140,8 +154,6 @@ async function run() {
           };
           const result = await userCollection.updateOne(filter, updateDoc);
           res.send(result);
-       
-      
   
       })
 
